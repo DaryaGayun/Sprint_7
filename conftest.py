1ba22f3048a2c2550503dd149a79320e9dc19bdc
Darya Gayun <darya.mihelson@mail.ru>
2026-09-09 15:52:17 +0300

    corrects 2

--- conftest.py
+++ conftest.py
@@ -2,13 +2,20 @@ import pytest
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from helpers import register_new_courier_and_return_login_password, login_courier, delete_courier, generate_random_string
-from helpers import delete_test_order
+from helpers import *
 
 @pytest.fixture
 def courier():
-    login_pass = register_new_courier_and_return_login_password()
+    login_pass = []
+    login = generate_random_string(10)
+    password = generate_random_string(10)
+    first_name = generate_random_string(10)
+    payload = {"login": login, "password": password, "firstName": first_name}
+    response = requests.post(Urls.CREATE_COURIER, data=payload)
+    if response.status_code == 201:
+        login_pass.append(login)
+        login_pass.append(password)
+        login_pass.append(first_name)
     login_resp = login_courier(login_pass[0], login_pass[1])
     courier_id = login_resp.json()["id"]
     yield {"login": login_pass[0], "password": login_pass[1], "id": courier_id}
--- helpers.py
+++ helpers.py
@@ -9,20 +9,6 @@ def generate_random_string(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 
-@step("Регистрация нового курьера")
-def register_new_courier_and_return_login_password():
-    login_pass = []
-    login = generate_random_string(10)
-    password = generate_random_string(10)
-    first_name = generate_random_string(10)
-    payload = {"login": login, "password": password, "firstName": first_name}
-    response = requests.post(Urls.CREATE_COURIER, data=payload)
-    if response.status_code == 201:
-        login_pass.append(login)
-        login_pass.append(password)
-        login_pass.append(first_name)
-    return login_pass
-
 @step("Логин курьера с логином {login}")
 def login_courier(login, password):
     payload = {"login": login, "password": password}
